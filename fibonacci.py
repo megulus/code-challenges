c46786c63fcc048b3e89931f5439e28e3802d5f1
Meg Dahlgren <meg.dahlgren@gmail.com>
2019-12-01 17:33:17 -0500

    Add iterative fibonacci method

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,6 @@
 import sys
 
+# recursive fibonacci
 def fib(n):
   if n == 0:
     return 0
@@ -8,8 +9,28 @@ def fib(n):
   else:
     return fib(n - 1) + fib(n -2)
 
+# iterative fibonacci
+def iterative_fib(n):
+  if n == 0:
+    return 0
+  # if n == 1:
+  #   return 1
+  else:
+    two_previous = 0
+    one_previous = 1
+    fib_num = 1
+    fib_result = 0
+    while fib_num < n:
+      fib_result = two_previous + one_previous
+      two_previous = one_previous
+      one_previous = fib_result
+      fib_num += 1
+    return fib_result
+
+
 def main():
-  print(fib(int(sys.argv[1])))
+  # print(fib(int(sys.argv[1])))
+  print(iterative_fib(int(sys.argv[1])))
 
 if __name__ == '__main__':
   main()
